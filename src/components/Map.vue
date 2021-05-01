e9d339f4b7b89affb273475b88460a5d638f668d
winstxnhdw <winstxnhdw@gmail.com>
2021-05-01 23:19:41 +0800

    big fix

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,33 +1,30 @@
 <template>
   <div id="map">
     <div id='vignette'> </div>
-    <l-map ref="map" :options="{attributionControl: false, zoomControl: false}" :zoom="zoom" :center="center" @click="createMarker">
+    <l-map ref="map" 
+      :options="{attributionControl: false, zoomControl: false}" 
+      :zoom="zoom" 
+      :center="center" 
+      @click="createMarker">
+
       <l-tile-layer :url="url"></l-tile-layer>
-      <l-polyline :lat-lngs="interpolate" :color="polyline.color" :opacity="polyline.opacity" :dashArray="polyline.dashArray"/>
+      <l-polyline 
+        :lat-lngs="interpolate" 
+        :color="polyline.color" 
+        :opacity="polyline.opacity" 
+        :dashArray="polyline.dashArray"/>
 
-      <v-marker-cluster :options="clusterOptions" @clusterclick="click()" @ready="ready">
-        <l-marker
-          :draggable="true" 
-          :key="index" 
-          :lat-lng="marker"
-          :icon="icon"
-          v-for="(marker, index) in markers" 
-          @click="deleteMarker(index)" 
-          @drag="updatePath($event, index)"
-          @dragend="updateLast($event, index)">
-        </l-marker>
-      </v-marker-cluster>
-
-      <!-- <v-marker-cluster :options="clusterOptions" @clusterclick="click()" @ready="ready">
-        <l-marker
-          :opacity=0.5
-          :draggable="false" 
-          :key="index" 
-          :lat-lng="marker"
-          :icon="icon"
-          v-for="(marker, index) in markers">
-        </l-marker>
-      </v-marker-cluster> -->
+      <l-marker
+        v-for="(marker, index) in markers" 
+        :key="index"
+        :draggable="true" 
+        :lat-lng="marker"
+        :icon="icon"
+        @click="deleteMarker(index)" 
+        @drag="updatePath($event, index)"
+        @dragstart="saveMarkerPos"
+        @dragend="updateLast($event, index)">
+      </l-marker>
 
       <l-control position="bottomleft" >
         <button type="button" class="btn btn-dark buttons" data-toggle="button" @click="modeSwitch"> {{mode}} </button>
@@ -40,7 +37,6 @@
 <script>
 import L from 'leaflet';
 import { LMap, LTileLayer, LMarker, LControl, LPolyline} from 'vue2-leaflet';
-import Vue2LeafletMarkercluster from 'vue2-leaflet-markercluster';
 import 'leaflet-draw';
 
 export default {
@@ -51,6 +47,9 @@ export default {
       center: L.latLng(1.331142, 103.774454),
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       markers: [],
+      selectedMarkers: [],
+      oldMarkerPos: [],
+      oldClickedMarkerPos: null,
       icon: L.icon({
         iconUrl: require('/src/assets/marker.svg'),
         iconSize: [20, 20],
@@ -76,8 +75,7 @@ export default {
     LTileLayer,
     LMarker,
     LControl,
-    LPolyline,
-    "v-marker-cluster": Vue2LeafletMarkercluster
+    LPolyline
     },
 
   methods: {
@@ -107,11 +105,51 @@ export default {
       }
     },
     updatePath(event, index) {
-      this.interpolate.splice(index, 1, [event.latlng.lat, event.latlng.lng]);
+      if(this.mode == 'Select Mode') {
+        var delta_lat = event.latlng.lat - this.oldClickedMarkerPos[0];
+        var delta_lng = event.latlng.lng - this.oldClickedMarkerPos[1];
+        
+        for(var m=0; m < this.selectedMarkers.length; m++) {
+          var selectedMarker = this.selectedMarkers[m];
+          this.interpolate[selectedMarker][0] = this.oldMarkerPos[m][0] + delta_lat;
+          this.interpolate[selectedMarker][1] = this.oldMarkerPos[m][1] + delta_lng;
+          this.markers[selectedMarker].lat = this.oldMarkerPos[m][0] + delta_lat;
+          this.markers[selectedMarker].lng = this.oldMarkerPos[m][1] + delta_lng;
+        }
+        this.interpolate.splice();
+      }
+
+      else {
+        this.interpolate.splice(index, 1, [event.latlng.lat, event.latlng.lng]);
+      }
     },
     updateLast(event, index) {
-      var latlng = event.target.getLatLng()
-      this.markers[index] = [latlng.lat, latlng.lng];
+      if(this.mode == 'Select Mode') {
+        console.log(event)
+      }
+
+      else {
+        var latlng = event.target.getLatLng();
+        this.markers[index].lat = latlng.lat;
+        this.markers[index].lng = latlng.lng;
+      }
+
+      this.mode = 'Create Mode'
+    },
+
+    saveMarkerPos(event) {
+      if(this.mode == 'Select Mode') {
+        var latlng = event.target.getLatLng();
+        this.oldMarkerPos = [];
+        this.oldClickedMarkerPos = [latlng.lat, latlng.lng];
+        
+        for(var i=0; i < this.selectedMarkers.length; i++) {
+          var selectedMarker = this.selectedMarkers[i];
+          var oldLat = this.markers[selectedMarker].lat;
+          var oldLng = this.markers[selectedMarker].lng;
+          this.oldMarkerPos.push([oldLat, oldLng]);
+        }
+      }
     },
     
     click: (e) => console.log("clusterclick", e),
@@ -154,7 +192,7 @@ export default {
         if(type === 'rectangle') {
           for(var i=0; i < this.markers.length; i++) {
             if(layer.getBounds().contains(this.markers[i]) == true) {
-              markersInside.push(this.markers[i]);
+              markersInside.push(i);
             }
           }
         }
@@ -162,16 +200,19 @@ export default {
         else if(type === 'circle') {
           for(var n=0; n < this.markers.length; n++) {
             if(layer.getLatLng().distanceTo(this.markers[n]) < layer.getRadius()) {
-              markersInside.push(this.markers[n]);
+              markersInside.push(n);
             }
           }
         }
+
+        this.mode = 'Select Mode';
+        this.selectedMarkers = markersInside;
 
         editableLayers.removeLayer(layer);
       });
 
       map.on('draw:toolbaropened', () => {
-        this.mode = 'Draw Mode'
+        this.mode = 'Draw Mode';
       });
     });
   }
@@ -199,8 +240,8 @@ export default {
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     box-shadow: inset 0 0 1000px black;
     z-index: 1000;
     pointer-events: none;
