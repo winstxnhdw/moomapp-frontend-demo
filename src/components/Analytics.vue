e054509a41c0ccf7b80f174952f7a56d47a1ab9d
winstxnhdw <winstxnhdw@gmail.com>
2021-05-10 04:43:59 +0800

    test

--- src/components/Analytics.vue
+++ src/components/Analytics.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div id="analyticsBar">
+    <b-nav tabs fill>
+      <b-nav-item active>Active</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Link with a long name </b-nav-item>
+      <b-nav-item disabled>Disabled</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
@@ -13,4 +20,18 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.h1 {
+  color: rgb(255, 255, 0);
+  font-size: 100px;
+  top: 100%;
+  position: fixed;
+  opacity: 1;
+}
+#analyticsBar {
+  position: fixed;
+  background-color: black;
+  height: 100%;
+  width: 20%;
+}
+</style>
