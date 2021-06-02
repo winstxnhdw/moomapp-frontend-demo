f92555f43b5a6c5bdc37fcb29681f452721b2b35
winstxnhdw <winstxnhdw@gmail.com>
2021-06-02 16:27:39 +0800

    fix margins

--- src/components/Configuration.vue
+++ src/components/Configuration.vue
@@ -4,7 +4,7 @@
       <v-card-text class="pl-0 pt-0">Import CSV files</v-card-text>
       <v-btn class="mb-3" :width="200" @click="importCSV">Import</v-btn>
     </v-card>
-    <v-card class="pa-3" min-height="100%">
+    <v-card class="pa-3 mt-3" min-height="100%">
       <v-card-text class="pl-0 pt-0">Export CSV files</v-card-text>
       <v-btn class="mb-3" :width="200">Export</v-btn>
     </v-card>
